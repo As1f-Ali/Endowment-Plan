e187b1156151ffce3e8c73c70d18547a78a79e58
As1f-Ali <asifali94851@gmail.com>
2025-07-08 12:48:31 +0530

    Variables

--- src/Variable.java
+++ src/Variable.java
@@ -5,15 +5,16 @@ public class Variable
     public static void main(String[] args)
     {
         Scanner input= new Scanner(System.in);
-        System.out.print("Enter your age:");
+        System.out.print("Enter your age: ");
         int age = input.nextInt();
         int num = 21;
         double num2 = 56.0f;
         String name = "Asif Ali";
-        char Character = 'A';
-        System.out.println(age);
-        System.out.println("Number is : " + num);
-        System.out.println(name);
-        System.out.println(Character);
+        char character = 'A';
+        System.out.println("Age is: " + age);
+        System.out.println("First number is : " + num);
+        System.out.println("Name: " + name);
+        System.out.println("Character: " + character);
+        System.out.printf("Second number is: " + num2);
     }
 }
